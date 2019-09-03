380c85fc328e07465a6719fac57027b533aeba22
John Hambry <johnphambry@gmail.com>
2019-09-03 17:39:47 -0500

    2.0

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("Legends Pub Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Smoke House Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Irish Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Brew and Gold Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Peanut Butter Burger");
